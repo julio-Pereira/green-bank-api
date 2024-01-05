4dd394bc224bdb6c2a4b41224e00d7c56ff9d0ed
julio-Pereira <jandrade.pereira1@gmail.com>
2024-01-05 18:43:50 -0300

    Added user's logic

---
+++ user/src/main/java/com/br/greenbank/dto/UpdateUserDto.java
@@ -0,0 +1,3 @@
+package com.br.greenbank.dto;
+
+public record UpdateUserDto(String fullName, String email, String password) {}
---
+++ user/src/test/java/UserTest.java
@@ -0,0 +1,64 @@
+import com.br.greenbank.domain.User;
+import com.br.greenbank.enums.CustomerType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserTest {
+
+    @Test
+    public void shouldCreateNewUserSuccessfully() {
+        User user = new User();
+        user.setFullName("John Doe");
+        user.setEmail("john.doe@email.com");
+        user.setPassword("123456");
+        user.setNationalId("123.456.789-01");
+
+        assertEquals("John Doe", user.getFullName());
+        assertEquals("john.doe@email.com", user.getEmail());
+        assertEquals("123456", user.getPassword());
+        assertEquals("123.456.789-01", user.getNationalId());
+        assertEquals(CustomerType.USER, user.getCustomerType());
+    }
+
+    @Test
+    public void shouldNotHaveDuplicatedUserEmails() {
+        User user = new User();
+        User user1 = new User();
+
+        user.setFullName("John Doe");
+        user.setEmail("john.doe@email.com");
+        user.setNationalId("123.456.789-01");
+        user.setPassword("123456");
+
+        user1.setFullName("John Doe Smith");
+        user1.setEmail("john.doe@email.com");
+        user1.setNationalId("123.456.789-02");
+        user1.setPassword("123456");
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            if (user.getEmail().equals(user1.getEmail())) throw new IllegalArgumentException("Email já cadastrado");
+        });
+    }
+
+    @Test
+    public void shouldNotHaveDuplicatedUserNationalIds() {
+        User user = new User();
+        User user1 = new User();
+
+        user.setFullName("John Doe");
+        user.setEmail("john.doe@email.com");
+        user.setNationalId("123.456.789-01");
+        user.setPassword("123456");
+
+        user1.setFullName("John Doe Smith");
+        user1.setEmail("john.smith@email.com");
+        user1.setNationalId("123.456.789-01");
+        user1.setPassword("123456");
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            if (user.getNationalId().equals(user1.getNationalId())) throw new IllegalArgumentException("CPF já cadastrado");
+        });
+    }
+}
